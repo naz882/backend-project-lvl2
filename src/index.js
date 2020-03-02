6e5a35d70d3446c246998dc19c6534008219bc93
Nazym <guliyev.n.d@gmail.com>
2020-03-02 03:56:40 +0000

    commander.js installation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("welcome");
+import program from 'commander';
+
+program.version('1.0');
+
+
+program
+  .option('-d, --description', 'description').action(()=> {console.log("This is description")});
+program
+  .option('-v, --version', 'version');
+program.option('-h, --help', 'help').action(()=> {
+
+    console.log('Compares two configuration files and shows a difference.');
+  
+    console.log('Options:')
+    console.log('  -V, --version        output the version number')
+    console.log('  -h, --help           output usage information')
+})
+program.parse(process.argv);
+
