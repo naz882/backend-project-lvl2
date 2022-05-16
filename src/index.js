92b10758d6c173c55058c41b8f33787b5769afa2
naz882 <guliyev.n.d@gmail.com>
2022-05-16 21:18:14 +0600

    typos fixed

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import readData from './parsers.js';
 export default (filepath1, filepath2, formatter = 'stylish') => {
   const data1 = readData(path.resolve(filepath1));
   const data2 = readData(path.resolve(filepath2));
-  console.log('SPARSILI DANNYE', data1, data2);
   const data = gendiff(data1, data2);
-  console.log("ETO DANNYE", data);
   return (format(data, formatter));
 };
